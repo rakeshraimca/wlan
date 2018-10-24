dc358b7609e853cadd7c7f79eb2f322acb7e99b0
sandyorton7 <sandyorton7@gmail.com>
2018-10-24 16:52:35 +0000

    temp

--- Android.mk
+++ Android.mk
@@ -1,8 +1,8 @@
-ifeq ($(call my-dir),$(call project-path-for,wlan))
+#ifeq ($(call my-dir),$(call project-path-for,wlan))
 
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 include $(call all-subdir-makefiles)
 endif
 
-endif
+#endif
